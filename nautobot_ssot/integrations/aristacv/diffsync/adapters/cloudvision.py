2271fcd879bbb5fb319525f21393bdb3cd78a1a7
gcastaldo <gerardo.castaldo@maticmind.it>
2023-12-11 18:29:14 +0100

    change

--- nautobot_ssot/integrations/aristacv/diffsync/adapters/cloudvision.py
+++ nautobot_ssot/integrations/aristacv/diffsync/adapters/cloudvision.py
@@ -193,7 +193,7 @@ class CloudvisionAdapter(DiffSync):
                 )
                 if self.job.debug:
                    self.job.logger.info(f"""IP Address interface {intf['interface']} on {dev.name} loaded.
-                    {new_ip.__getattribute__} """)
+                    {new_ip.json} """)
                 try:
                     self.add(new_ip)
                 except ObjectAlreadyExists as err:
